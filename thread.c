38b7254273e73e8b3d4a9e18fde83708ff89d547
Jialin Li <jialin_li@utexas.edu>
2018-04-09 22:40:30 -0500

    thread blocks on empty task

--- thread.c
+++ thread.c
@@ -1,7 +1,9 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <errno.h>
+#include<stdlib.h>
+#include<stdio.h>
 #include <sys/sysinfo.h>
 #include "thread.h"
+
 
 // will probably change to list when the thread number becomes dynamic
 Thread* thread_info;
@@ -9,6 +11,8 @@ int workers;
 int mutex_flag;
 
 void* worker_func(void* t);
+void lock(Thread* t);
+void unlock(Thread* t);
 
 void thread_pool_init(int w, int use_mutex) {
   mutex_flag = use_mutex;
@@ -27,6 +31,11 @@ void thread_pool_init(int w, int use_mutex) {
   int e;
   // set up info struct for each thread
   for (int i=0; i<workers; i++) {
+    if ((e=sem_init(&thread_info[i].sema, PTHREAD_PROCESS_PRIVATE, 0)) != 0) {
+      printf("failed to initialize the semaphore for thread %d, error code %d\n", i, e);
+      continue;
+    }
+
     // initialize both just in case
     if ((e=pthread_mutex_init(&thread_info[i].mutex, NULL)) != 0) {
       printf("failed to initialize the mutex for thread %d, error code %d\n", i, e);
@@ -44,74 +53,82 @@ void thread_pool_init(int w, int use_mutex) {
   }
 }
 
-Task* task_init(void *func, void* aux) {
-    assert(func != NULL && aux != NULL);
-    Task *new_task = (Task *) malloc(sizeof(Task));
-    if (!new_task) {
-        printf("Task initialization failed\n");
-        return NULL;
-    }
-    // initialize task's attributes
-    new_task->func = func;
-    new_task->aux = aux;
-    // These two variable will be updated when they are added to a thread's queue
-    new_task->prev = NULL;
-    new_task->next = NULL;
-    return new_task;
+Task* task_init(task_func *func, void* aux) {
+  if (func == NULL)
+    return NULL;
+
+  Task *new_task = (Task *) malloc(sizeof(Task));
+  if (!new_task) {
+    printf("Task initialization failed\n");
+    return NULL;
+  }
+  
+  // initialize task's attributes
+  new_task->func = func;
+  new_task->aux = aux;
+  // These two variable will be updated when they are added to a thread's queue
+  new_task->prev = NULL;
+  new_task->next = NULL;
+  return new_task;
 }
 
+// should be used as an internal function
 void task_add(Task* task, Thread *thread) {
-    assert(task != NULL && thread != NULL);
-    lock(thread);
-    // if the task queue is initially empty 
-    if (thread->task_queue == NULL) {
-        thread->task_queue = task;
-        thread->last_task = task;
-        unlock(thread);
-        return;
-    }
-    task->prev = thread->last_task;
-    thread->last_task->next = task;
+  if (task == NULL || thread == NULL)
+    return;
+  
+  lock(thread);
+  thread->total_tasks++;
+
+  // if the task queue is initially empty 
+  if (thread->task_queue == NULL) {
+    thread->task_queue = task;
     thread->last_task = task;
-    total_tasks ++:
     unlock(thread);
+    return;
+  }
+
+  task->prev = thread->last_task;
+  thread->last_task->next = task;
+  thread->last_task = task;
+  unlock(thread);
 }
 
 // TODO so far we only have a single queue on each thread to keep track
 // of all tasks assigned to it, but later we might have multiple queues
 // needed to track task at different states such as blocked etc.
-void task_remove(Thread *thread) {
-    // currently we are
-    assert(thread != NULL);
-    lock(thread);
-    if (thread->last_task == NULL) {
-        unlock(thread);
-        return;
-    }
+// void task_remove(Thread *thread) {
+//     // currently we are
+//     assert(thread != NULL);
+//     lock(thread);
+//     if (thread->last_task == NULL) {
+//         unlock(thread);
+//         return;
+//     }
     
-    // The task we remove can be the only one left in the queue
-    if (thread->task_queue == thread->last_task) {
-        Task *task_to_remove = thread->last_task;
-        thread->last = NULL;
-        thread->task_queue = NULL;
-        task_free(task_to_remove);
-        unlock(thread);
-        return;
-    }
+//     // The task we remove can be the only one left in the queue
+//     if (thread->task_queue == thread->last_task) {
+//         Task *task_to_remove = thread->last_task;
+//         thread->last = NULL;
+//         thread->task_queue = NULL;
+//         task_free(task_to_remove);
+//         unlock(thread);
+//         return;
+//     }
 
-    // normal situation
-    Task *task_to_remove = thread->last_task;
-    thread->last_task = thread->last_task->prev;
-    thread->last_task->next = NULL;
-    task_free(task_to_remove);
-    unlock(thread);
-}
+//     // normal situation
+//     Task *task_to_remove = thread->last_task;
+//     thread->last_task = thread->last_task->prev;
+//     thread->last_task->next = NULL;
+//     task_free(task_to_remove);
+//     unlock(thread);
+// }
 
-bool task_free(Task *task) {
-    // need to clean up the mess in thread
-    assert(task != NULL);
-    free((void *) task);
-}
+// bool task_free(Task *task) {
+//     // need to clean up the mess in thread
+//     assert(task != NULL);
+//     free((void *) task);
+// }
 
 void lock(Thread* t) {
   int e = 0;
@@ -140,6 +157,7 @@ void unlock(Thread* t) {
 }
 
 void* worker_func(void* t) {
+  int e;
   Thread* info = (Thread*) t;
   printf("thread %lu is up and running \n", info->tid);
 
@@ -149,7 +167,14 @@ void* worker_func(void* t) {
   while(1) {
     // wait for jobs to come in. busy waiting for now
     // TODO: think about blocking/parking policy
-    while (info->task_queue == NULL);
+    // while (info->task_queue == NULL);
+
+    // NOTE: need to consider the interaction of sem_wait and 
+    // future work stealing algorithm
+    if ((e=sem_wait(&info->sema)) != 0) {
+      printf("Failed to wait for task, error %d, will keep trying \n", errno);
+      continue;
+    }
     lock(info);
     // state might have changed already
     task = info->task_queue;
@@ -159,6 +184,7 @@ void* worker_func(void* t) {
     // task may or may not exist
     if (task) {
       task->func(task->aux);
+      free(task);
     }
   }
 
--- thread.h
+++ thread.h
@@ -1,10 +1,11 @@
-#include <pthread.h>
+#include<pthread.h>
+#include <semaphore.h>
 
 typedef void task_func (void *aux);
 
 typedef struct task {
   void*        aux;
-  ask_func*   func;
+  task_func*   func;
   struct task* prev;
   struct task* next;
 } Task;
@@ -13,14 +14,14 @@ typedef struct thread {
   pthread_t          tid;
   pthread_spinlock_t lock;        // guard access to task_queue
   pthread_mutex_t    mutex;
+  sem_t sema; // use to notify task is there
   int                total_tasks; // number of tasks assigned to this thread TODO atomic maybe?
   Task*              task_queue;
   Task*              last_task;
 } Thread;
 
-void thread_pool_init(int workers);
-bool task_init();
-bool add_task_to_thread(pthread_t tid, int num_tasks);
+Task* task_init(task_func *func, void* aux);
+// bool add_task_to_thread(pthread_t tid, int num_tasks);
 
 void thread_pool_init(int workers, int mutex_flag);
 void thread_pool_wait();
